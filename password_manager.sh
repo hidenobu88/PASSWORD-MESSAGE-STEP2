6f0ebe200bbc787fa628cc854e59d7ed8d8c2213
hidenobu88 <hidenobu0846@gmail.com>
2024-01-12 14:05:28 +0000

    Get-Passwordが選択された場合の処理コード

--- password_manager.sh
+++ password_manager.sh
@@ -13,19 +13,44 @@ while true; do
 # ユーザーからの入力を受け取りは、変数 selectで対応 
   read select
 
-# シェルスクリプトでのcase文のルールは「esac」で最後閉じる
+# シェルスクリプトでのcase文の閉じタグは「esac」
 
   case $select in
 
+     # Add Passwordが選択された場合の処理コード
+         "Add Password")
+         read -p "サービス名を入力してください: " ServiceName
+	 read -p "ユーザー名を入力してください: " UserName
+	 read -p "パスワードを入力してください: " PassWord
+
+        # 入力された情報を保存する処理と成功のお知らせを表示
+	   echo "$ServiceName:$UserName:$PassWors" >> password_manager.sh
+           echo "パスワードの追加は成功しました。"
+	   ; 
+
+     # Get Passwordが選択された場合の処理コード
+         "Get Password")
+         read -p "サービス名を入力してください: " ServiceName
+         
+              # ServiceName変数に対応するpassをファイルから取得
+	      # grepはファイル中の文字列を検索するコマンド
+	      # cutはテキストファイルを横方向に分割するコマンド
+              PassWord=$(grep "^$ServiceName:" password_manager.sh | cut -d: -f3)
 
 
-
-
-
-
-
-
-
+              # サービス名が登録されていなかった場合の処理はif文
+              # シェルスクリプトでのif文の閉じタグは「fi」
+         
+	      if [ -z "$PassWord" ]; then
+                  echo "そのサービスは登録されていません。"
+              else
+                  echo "サービス名：$ServiceName"
+                  # 保存されている情報が見つかれば表示
+                  echo "ユーザー名：$(grep "^$ServiceName:" password_manager.sh | cut -d: -f2)"
+                  # 保存されている情報が見つかれば表示
+                  echo "パスワード：$password"
+              fi
+              ;
 
 
 
