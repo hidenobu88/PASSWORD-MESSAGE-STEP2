e6bf34341f5961a97e72f5b323de29c180fd73f6
hidenobu88 <hidenobu0846@gmail.com>
2024-01-12 12:38:10 +0000

    case文と受け取り変数の設定

--- password_manager.sh
+++ password_manager.sh
@@ -2,21 +2,50 @@
 
 
 # 今回の仕様書（Quest）には、EXITが入力されるまでプログラムは終了しないとあるので「while文のdo〜done」を使用
-
 # trueは条件が満たされる間、ループするという意味
 
 while true; do
 
-
- echo "パスワードマネージャーへようこそ！"
-    echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
-    # ユーザーからの入力を受け取りは、変数 selectで対応 
-    read select
+  echo "パスワードマネージャーへようこそ！"
+  echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
 
 
+# ユーザーからの入力を受け取りは、変数 selectで対応 
+  read select
+
+# シェルスクリプトでのcase文のルールは「esac」で最後閉じる
+
+  case $select in
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  esac
 
 
 
