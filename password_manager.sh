6c06179b7c8ec26ddbd2cfa1470c3529fc42ca27
hidenobu88 <hidenobu0846@gmail.com>
2024-01-12 13:24:51 +0000

    Add-Passwordが選択された場合の処理コード

--- password_manager.sh
+++ password_manager.sh
@@ -14,8 +14,18 @@ while true; do
   read select
 
 # シェルスクリプトでのcase文のルールは「esac」で最後閉じる
-
   case $select in
+     
+     # Add Passwordの処理（read関数で入力受付の変数も設定）
+     "Add Password")
+        read -p "サービス名を入力してください: " ServiceName
+	read -p "ユーザー名を入力してください: " UserName
+	read -p "パスワードを入力してください: " PassWord
+
+	# 入力された情報を保存する処理
+	echo "$ServiceName:$UserName:$PassWors" >> password_manager.sh
+        echo "パスワードの追加は成功しました。"
+	;
 
 
 
@@ -26,62 +36,9 @@ while true; do
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
+ 
   esac
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
